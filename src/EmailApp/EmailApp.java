3d837f46b112630ab3f7872eefb502db5ba91e2c
Anon <thunderouspeanut@gmail.com>
2024-03-10 03:34:32 +0530

    some updates...again

--- src/EmailApp/EmailApp.java
+++ src/EmailApp/EmailApp.java
@@ -5,7 +5,6 @@ public class EmailApp {
         Email email = new Email("John", "Doe");
         email.setMailboxCapacity(500);
         email.setAlternateEmail("abc@pqr.xyz.com");
-        System.out.println("------------------------------");
         System.out.println(email.showInfo());
     }
 }
